3f5750f223742db8aefc95b15fc0d875e76f8d06
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-10 09:37:33 +0300

    Add contact index route, controller, model, table contacts, migration

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Post routes
 Route::get('/posts', 'PostController@index')->name('post.index');
 Route::get('/post/create', 'PostController@create')->name('post.create');
 Route::get('/posts/{post}', 'PostController@show')->name('post.show');
 Route::get('/posts/{post}/edit', 'PostController@edit')->name('post.edit');
 Route::patch('/posts/{post}', 'PostController@update')->name('post.update');
 Route::delete('/posts/{post}', 'PostController@destroy')->name('post.destroy');
-
 Route::post('/posts', 'PostController@store')->name('post.store');
+
+//Contact routes
+Route::get('/contacts', 'ContactController@index')->name('contacts.index');
+
 
 
 Route::get('/about', function (){
     return view('about');
 })->name('about.index');
-Route::get('/contacts', function (){
-    return view('contacts');
-})->name('contacts.index');
+
