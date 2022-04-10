4bde42d89f9245e19c4f265e55d9282d44c13d4a
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-10 06:31:45 +0300

    Add placeholder to create form

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,11 +5,11 @@
         <form>
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" name="title" class="form-control" id="title">
+                <input type="text" name="title" class="form-control" id="title" placeholder="Title">
                 <label for="content" class="form-label">Content</label>
-                <textarea id="content" name="content" class="form-control"></textarea>
+                <textarea id="content" name="content" class="form-control" placeholder="Content"></textarea>
                 <label for="image" class="form-label">Image</label>
-                <input type="text" name="image" class="form-control" id="image">
+                <input type="text" name="image" class="form-control" id="image" placeholder="Image">
             </div>
             <button type="submit" class="btn btn-primary">Create</button>
         </form>
