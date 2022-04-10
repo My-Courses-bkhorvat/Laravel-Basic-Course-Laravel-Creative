5b81809687d5849189c22327068f0d31d007f7ea
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-10 06:36:25 +0300

    Edit rout store

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/posts', 'PostController@index')->name('post.index');
 Route::get('/post/create', 'PostController@create')->name('post.create');
 Route::get('/posts/{post}', 'PostController@show')->name('post.show');
 
-Route::post('/posts/store', 'PostController@store')->name('post.store');
+Route::post('/posts', 'PostController@store')->name('post.store');
 
 
 Route::get('/about', function (){
