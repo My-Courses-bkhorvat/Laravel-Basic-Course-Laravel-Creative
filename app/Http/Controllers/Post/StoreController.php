76140b2dfcdd4ea76a846afb3d527f7356ea510b
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-14 20:16:15 +0200

    Use request class for posts

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -3,20 +3,14 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Post;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'content' => 'required|string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => ''
-        ]);
-
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
 
