37d6e196a793e5b6a64df2eed6115d3d631e37ca
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-14 21:26:20 +0200

    Add Service class

--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -2,20 +2,15 @@
 
 namespace App\Http\Controllers\Post;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\UpdateRequest;
 use App\Post;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(UpdateRequest  $request, Post $post)
     {
         $data = $request->validated();
-        $tags = $data['tags'];
-        unset($data['tags']);
-
-        $post->update($data);
-        $post->tags()->sync($tags);
+        $this->service->update($post, $data);
         return redirect()->route('post.show', $post->id);
     }
 }
