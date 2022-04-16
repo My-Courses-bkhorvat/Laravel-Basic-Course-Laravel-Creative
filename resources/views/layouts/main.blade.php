e7facd94dfc6961b94f21b4035282544868c95c0
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-16 21:26:15 +0200

    Add just Policy class

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,61 +5,72 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('main.index') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contacts.index') }}">Contacts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('photos.index') }}">Photos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('friends.index') }}">Friends</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('tasks.index') }}">Tasks</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('skills.index') }}">Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('days.index') }}">Days</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('habits.index') }}">Habits</a>
+                        </li>
+
+                        @can('view', auth()->user())
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('main.index') }}">Home</a>
+                                <a class="nav-link" href="{{ route('admin.post.index') }}">Admin</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('contacts.index') }}">Contacts</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('photos.index') }}">Photos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('friends.index') }}">Friends</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('tasks.index') }}">Tasks</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('skills.index') }}">Skills</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('days.index') }}">Days</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('habits.index') }}">Habits</a>
-                            </li>
-                            <li class="nav-item">
-                               @yield('add_button')
-                            </li>
-                        </ul>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
+                        @endcan
+                        <li class="nav-item">
+                            @yield('add_button')
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     </div>
+</div>
 @yield('content')
 
 </body>
