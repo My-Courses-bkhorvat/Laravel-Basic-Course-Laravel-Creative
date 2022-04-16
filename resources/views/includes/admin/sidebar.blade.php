4dbe545776859665d6900c908a086a51857ef3eb
bkhorvatSisterPC <bkhorvat.php@gmail.com>
2022-04-16 18:45:22 +0200

    Fix href route to posts on admin panel

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-header">ADMIN PANEL</li>
         <li class="nav-item">
-            <a href="pages/calendar.html" class="nav-link">
+            <a href="{{ route('admin.post.index') }}" class="nav-link">
                 <i class="nav-icon fas fa-solid fa-align-justify"></i>
                 <p>
                     Posts
